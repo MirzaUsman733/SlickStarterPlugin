b8e620e71147b617ef718afe327967675badebea
MirzaUsman733 <125288010+MirzaUsman733@users.noreply.github.com>
2023-12-24 18:42:44 +0500

    first commit

---
+++ src/utils/db.js
@@ -0,0 +1,38 @@
+// import mongoose from "mongoose";
+
+// const connect = async () => {
+//   if (mongoose.connections[0].readyState) return;
+
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Mongo Connection successfully established.");
+//   } catch (error) {
+//     throw new Error("Error connecting to Mongoose");
+//   }
+// };
+
+// export default connect;
+
+import mongoose from "mongoose";
+
+const connect = async () => {
+  try {
+    if (mongoose.connection.readyState !== 1) {
+      await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Mongo Connection successfully established.");
+    } else {
+      console.log("MongoDB is already connected.");
+    }
+  } catch (error) {
+    console.error("Error connecting to Mongoose:", error.message);
+    throw new Error("Error connecting to Mongoose");
+  }
+};
+
+export default connect;
