ed654b427577ec1bc444daf18ab07e33cdb91017
MirzaUsman733 <125288010+MirzaUsman733@users.noreply.github.com>
2023-12-28 20:27:48 +0500

    After comment resolve error

---
+++ src/components/UserDataDashboard.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+interface UserData {
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
+const UserDataDashboard: React.FC = () => {
+  const [userData, setUserData] = useState<UserData[]>([]);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/users", {
+          method: "POST",
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data: { userData: UserData[] } = await response.json();
+        setUserData(data.userData);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleUserClick = (userDataInfo: string) => {
+    router.push(`/dashboard/${userDataInfo}`);
+  };
+
+  return (
+    <div>
+      <div className="grid place-items-center h-screen">
+        <div className="shadow-2xl shadow-slate-700 text-black p-8 bg-opacity-20 bg-zince-300/10 flex flex-col gap-2 my-6">
+          <h1 className="text-2xl font-bold mb-4">All Users</h1>
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead>
+              <tr>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Name
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Email
+                </th>
+                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Role
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {userData.map((user) => (
+                <tr key={user._id} onClick={() => handleUserClick(user._id)}>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm font-medium text-gray-900">
+                      {user?.name}
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm font-medium text-gray-900">
+                      {user?.email}
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm font-medium text-gray-900">
+                      {user?.role}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserDataDashboard;
